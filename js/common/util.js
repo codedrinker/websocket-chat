665276e1148bb96896506a28d4168c53adcf2e63
unierschunlei <chunlei.wang@uniers.com>
2014-07-21 13:13:29 +0800

    add spin ,spin is plugins to  dynamically creates spinning activity indicators that can be used as resolution-independent replacement for AJAX loading GIFs.

--- js/common/util.js
+++ js/common/util.js
@@ -23,3 +23,25 @@ function jsonAppend(j1, j2) {
 				+ j2s.slice(1, +j2s.length));
 	}
 };
+function showspin(id){
+	var opts = {
+		lines: 11, // The number of lines to draw
+		length: 17, // The length of each line
+		width: 10, // The line thickness
+		radius: 30, // The radius of the inner circle
+		corners: 1, // Corner roundness (0..1)
+		rotate: 26, // The rotation offset
+		direction: 1, // 1: clockwise, -1: counterclockwise
+		color: '#000', // #rgb or #rrggbb or array of colors
+		speed: 1, // Rounds per second
+		trail: 60, // Afterglow percentage
+		shadow: false, // Whether to render a shadow
+		hwaccel: false, // Whether to use hardware acceleration
+		className: 'spinner', // The CSS class to assign to the spinner
+		zIndex: 2e9, // The z-index (defaults to 2000000000)
+		top: '50%', // Top position relative to parent
+		left: '50%' // Left position relative to parent
+		};
+	var target = document.getElementById(id);
+	var spinner = new Spinner(opts).spin(target);
+}
